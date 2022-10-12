bc8d52247d0649b74644807e32f29a5c158d470c
1986sami <smmorales32@misena.edu.co>
2022-10-12 08:04:39 -0500

    primer commit
    primer commit

---
+++ back/server.js
@@ -0,0 +1,14 @@
+const app=require("./app")
+app.listen(process.env.PORT)
+
+
+//Setear el archivo de configuración
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+
+
+//Llamemos al server
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
