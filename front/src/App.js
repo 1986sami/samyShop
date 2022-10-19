c338bae74cce9a5f65e46c6c08dae47d2e9e7c25
1986sami <smmorales32@misena.edu.co>
2022-10-19 15:42:14 -0500

    commi react
    commit react

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React  from 'react';//para compilar react
+
+function App() {
+  return (
+    <div className="App"> 
+    <h1>Bienvenidos</h1>
+    <img src="./images/samyshop.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
