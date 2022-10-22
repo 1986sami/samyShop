569ac32f9226ca7f86c7d32579640c66534ea97b
1986sami <smmorales32@misena.edu.co>
2022-10-22 08:07:03 -0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -6,6 +6,8 @@ import store from './store';
 import { positions, transitions, Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
+
+
 const options = {
   timeout: 3000,
   position: positions.BOTTOM_CENTER,
